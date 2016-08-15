c93591926cc0545ab6261528252b1c866f47ff06
Taras Malynovsky <malynovsky@ukr.net>
2016-08-15 10:45:14 +0300

    15.08.16: Max. значення таймерів павзи/роботи ВФ і ЗНмакс збільшено до 300с

--- working files/inc/const_septoints_interval.h
+++ working files/inc/const_septoints_interval.h
@@ -236,10 +236,10 @@
 #define SETPOINT_UMAX2_MAX            140000
 
 #define TIMEOUT_UMAX1_MIN             0
-#define TIMEOUT_UMAX1_MAX             32000
+#define TIMEOUT_UMAX1_MAX             300000
 
 #define TIMEOUT_UMAX2_MIN             0
-#define TIMEOUT_UMAX2_MAX             32000
+#define TIMEOUT_UMAX2_MAX             300000
 
 #define SETPOINT_AVR1_MIN             10000
 #define SETPOINT_AVR1_MAX             150000
@@ -394,10 +394,10 @@
 #define NUMBER_DEFINED_NOT_MAX        NUMBER_DEFINED_NOT
 
 #define TIMEOUT_DF_PAUSE_MIN          0
-#define TIMEOUT_DF_PAUSE_MAX          32000
+#define TIMEOUT_DF_PAUSE_MAX          300000
 
 #define TIMEOUT_DF_WORK_MIN           0
-#define TIMEOUT_DF_WORK_MAX           32000
+#define TIMEOUT_DF_WORK_MAX           300000
 
 #define TIMEOUT_PREFAULT_MIN          100
 #define TIMEOUT_PREFAULT_MAX          5000
