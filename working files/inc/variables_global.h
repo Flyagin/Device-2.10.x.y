867fa25fdc84adcbcb2b687f1017626f6d168550
Taras Malynovsky <malynovsky@ukr.net>
2016-08-16 08:47:47 +0300

    16.08.16: підправив коорлинати базисів для визначення правильної послідовності фаз

--- working files/inc/variables_global.h
+++ working files/inc/variables_global.h
@@ -245,8 +245,8 @@ unsigned int information_about_clean_energy = 0;
 
 #define VAGA_E_AMPLITUDE        7
 const int ea[2] = {(int)(((float)(1 << VAGA_E_AMPLITUDE))*( 0.0000f)), (int)(((float)(1 << VAGA_E_AMPLITUDE))*( 1.0000f))};
-const int eb[2] = {(int)(((float)(1 << VAGA_E_AMPLITUDE))*(-0.9511f)), (int)(((float)(1 << VAGA_E_AMPLITUDE))*(-0.3090f))};
-const int ec[2] = {(int)(((float)(1 << VAGA_E_AMPLITUDE))*( 0.9511f)), (int)(((float)(1 << VAGA_E_AMPLITUDE))*(-0.3090f))};
+const int eb[2] = {(int)(((float)(1 << VAGA_E_AMPLITUDE))*(-0.8660f)), (int)(((float)(1 << VAGA_E_AMPLITUDE))*(-0.5000f))};
+const int ec[2] = {(int)(((float)(1 << VAGA_E_AMPLITUDE))*( 0.8660f)), (int)(((float)(1 << VAGA_E_AMPLITUDE))*(-0.5000f))};
 #undef  VAGA_E_AMPLITUDE
 unsigned int sequence_TN1 = 0;
 unsigned int sequence_TN2 = 0;
